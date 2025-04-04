6f487722570af5a1d5147d748d07ba65159012ef
ChengLin <146141054+Paoswrq@users.noreply.github.com>
2025-04-03 21:24:09 -0400

    Update WaveForm.js

--- js/WaveForm.js
+++ js/WaveForm.js
@@ -8,7 +8,7 @@ startMicrophoneAnalysis();
 export function loopWave() {
     let hue = getPitch(); 
     let dataArray = getAudioData();
-    ctx.fillStyle = "hsl(" + (hue + 180) + ", 50%, 30%)";
+    ctx.fillStyle = "hsl(" + hue + ", 50%, 30%)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     let waveConfig = [
@@ -24,11 +24,11 @@ export function loopWave() {
         let previousY = 0; 
         
         let gradient = ctx.createLinearGradient(0,0,canvas.width,0);
-        gradient.addColorStop(0,  "hsl(" + hue * 1.00 + ", 70%, 50%)")
-        gradient.addColorStop(0.25,  "hsl(" + hue * 0.98 + ", 70%, 50%)")
-        gradient.addColorStop(0.5,  "hsl(" + hue * 0.86 + ", 70%, 50%)")
-        gradient.addColorStop(0.75,  "hsl(" + hue * 0.64 + ", 70%, 50%)")
-        gradient.addColorStop(1,  "hsl(" + hue * 0.42 + ", 70%, 50%)")
+        gradient.addColorStop(0,  "hsl(" + (hue + 180) * 1.00 + ", 70%, 50%)")
+        gradient.addColorStop(0.25,  "hsl(" + (hue + 180) * 0.98 + ", 70%, 50%)")
+        gradient.addColorStop(0.5,  "hsl(" + (hue + 180) * 0.86 + ", 70%, 50%)")
+        gradient.addColorStop(0.75,  "hsl(" + (hue + 180) * 0.74 + ", 70%, 50%)")
+        gradient.addColorStop(1,  "hsl(" + (hue + 180) * 0.52 + ", 70%, 50%)")
 
         for (let i = 0; i < dataArray.length; i++) {
           let v = (dataArray[i] - 128) / 128; 
